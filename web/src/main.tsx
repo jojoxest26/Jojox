c60269a4bb1edfb5958548c6af49d06e4e8fb489
jojoxest26 <jojoxest26@gmail.com>
2026-08-29 20:17:02 +0200

    Commit 3 files to claude/banca-italia

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.tsx'
 import { LanguageProvider } from './i18n/LanguageContext.js'
+
+inject()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
